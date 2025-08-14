07538cf39df3f1c58cd001e08f5f8cf10b96669e
Divyanshi017 <divyanshisagar1742007@gmail.com>
2025-08-14 15:16:20 +0530

    Pattern1

---
+++ p1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void p1(){
+    for(int i=0;i<4;i++) {
+        for(int j=0;j<4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    p1();
+}
