ec151829939ba5a48a29a7443ab242dac8a64e16
Divyanshi017 <divyanshisagar1742007@gmail.com>
2025-08-15 15:40:17 +0530

    Pattern2

---
+++ p2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void p2(){
+    for(int i=1;i<=5;i++){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+int main(){
+    p2();
+}
