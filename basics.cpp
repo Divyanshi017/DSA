459f8a854dd808119e143a76452586b86d70f735
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-11 21:59:34 +0530

    vectors

--- basics.cpp
+++ basics.cpp
@@ -32,27 +32,52 @@ using namespace std;
 
 
 
-// Pair in STL
+// Pair in STL- it is a container that can hold two values of different data types
 
-void pairss(){
-    // pair<int, int> p= {1,3};
-    // // cout<< p.first<< " "<< p.second;
-    // pair<int, pair<int,int>> p2={1,{2,3}};
-    // // cout<< p2.first<<" "<<p2.second.first<<" "<<p2.second.second;
-    pair<int,int> arr[]={{1,2},{3,4},{5,6}};
-    cout<< arr[0].first;
+// void pairss(){
+//     // pair<int, int> p= {1,3};
+//     // // cout<< p.first<< " "<< p.second;
+//     // pair<int, pair<int,int>> p2={1,{2,3}};
+//     // // cout<< p2.first<<" "<<p2.second.first<<" "<<p2.second.second;
+//     pair<int,int> arr[]={{1,2},{3,4},{5,6}};
+//     cout<< arr[0].first;
 
-}
+// }
+
+// int main(){
+//     pairss();
+// }
+
+
+
+
+
+
+
+// Vectors in STL- it is also a container like array but with dynamic size
+
+// void vectors(){
+//     vector<int> v;
+//     v.push_back(1);
+//     v.emplace_back(2);
+//     // cout<< v[0]<< " "<< v[1]<< endl;
+
+//     vector<pair<int,int>> vec;
+//     vec.push_back({1,2});
+//     vec.emplace_back(3,4);
+//     cout<< vec[0].first<< " " << vec[0].second<< " " << vec[1].first<< " " << vec[1].second<< endl;
+
+    
+// }
+
+
+
+
+
+
+
+
 
 int main(){
-    pairss();
+    vectors();
 }
-
-
-
-
-
-
-
-// Vectors in STL
-
