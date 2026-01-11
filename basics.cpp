4fb24e1f97bfaee263cecd25f6686e7036c4de82
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-11 16:57:29 +0530

    pairs

--- basics.cpp
+++ basics.cpp
@@ -1,6 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;    
-int main(){
-    cout<<"Hello, World!"<<endl;
-    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// STL
+// 1. Algorithms
+// 2. Containers
+// 3. Functions
+// 4. Iterators
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pair in STL
+
+void pairss(){
+    // pair<int, int> p= {1,3};
+    // // cout<< p.first<< " "<< p.second;
+    // pair<int, pair<int,int>> p2={1,{2,3}};
+    // // cout<< p2.first<<" "<<p2.second.first<<" "<<p2.second.second;
+    pair<int,int> arr[]={{1,2},{3,4},{5,6}};
+    cout<< arr[0].first;
+
 }
+
+int main(){
+    pairss();
+}
+
+
+
+
+
+
+
+// Vectors in STL
+
