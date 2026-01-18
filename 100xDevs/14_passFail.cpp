0cbf2e35b2e1d44066b27b8d34d3fda5ca5c58cd
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-18 22:20:39 +0530

    passFail

---
+++ 100xDevs/14_passFail.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >>n;
+    if(1<=n && n<=100){
+        if(n<=35)
+        cout<< "Fail"<< endl;
+    else
+        cout<<"Pass"<<endl;
+    }
+}
