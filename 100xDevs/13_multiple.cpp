7945157f507dfce356ba3a1edca3d5ed5cd34a82
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-18 22:20:52 +0530

    multiple

---
+++ 100xDevs/13_multiple.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    if(1<=n && n<=1000000000 && 1<=m && m<=1000000000){
+        if(m%n == 0)
+            cout<< "Yes"<< endl;
+        else
+            cout<< "No"<< endl;
+    }
+}
