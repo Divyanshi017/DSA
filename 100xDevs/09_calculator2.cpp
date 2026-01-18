51406b248e4f9ba705af9c00b34bf18e300b5308
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-18 18:50:13 +0530

    calculator2

---
+++ 100xDevs/09_calculator2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long n;
+    long long m;
+    cin >> n;
+    cin >> m;
+    if(1<=n && m<=1000000000){
+        cout << n << " + " << m << " = " << n+m << endl;
+        cout << n << " - " << m << " = " << n-m << endl;
+        cout << n << " * " << m << " = " << n*m << endl;
+        cout << n << " / " << m << " = " << n/m << endl;
+        cout << n << " % " << m << " = " << n%m << endl;
+    }
+}
