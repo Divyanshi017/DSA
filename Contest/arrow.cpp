cdbbaf8060da3ad3db6cf72b91ffef22576a1984
Divyanshi <divyanshisagar1742007@gmail.com>
2026-01-29 21:09:22 +0530

    100xDevs

---
+++ Contest/arrow.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >>n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        cout << ">" << endl;
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        cout << ">" << endl;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++){
+            if(i>=j || i<=i*2)
+                cout << " ";
+            else if(i<j || i>i*2)
+                cout << " ";
+        }
+    }
+}
